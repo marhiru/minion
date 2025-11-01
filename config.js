4562faf0c73e9b13efa6efb7fbc8644a6c619bed
marhiru <marcosgamern@gmail.com>
2025-11-01 16:19:10 -0300

    fix: fixed

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     helpCmdPerPage: 10, //- Number of commands per page of help command
     lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
     adminId: "852599498323787838", //- Replace UserId with the Discord ID of the admin of the bot
-    token: process.env.token, //- Bot's Token
+    token: "asdok", //- Bot's Token
     clientId: process.env.clientId || "1200862192669577467", //- ID of the bot
     clientSecret: process.env.clientSecret || "hfsCSMMMq2Y84eiJD_hOk8acG3RL2-Y1", //- Client Secret of the bot
     port: 4200, //- Port of the API and Dashboard
